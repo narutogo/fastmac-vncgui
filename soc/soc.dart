edfaa2e745166806ad1ad55b8875dbadcb04c2f0
narutogo <36765196+narutogo@users.noreply.github.com>
2024-07-23 16:26:19 +0800

    Update soc.dart

--- soc/soc.dart
+++ soc/soc.dart
@@ -5,8 +5,8 @@ import 'dart:io';
 import 'package:process_run/shell.dart';
 
 class Com {
-  static  WebSocket? webk;
-  static  Timer? timer;
+   late WebSocket webk;
+   late Timer timer;
   static Future<void> send(Map map) async {
     var no = 'vps';
     map['name'] = no;
